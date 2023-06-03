effebbf30863390622bb48e9c2833332703fc1e7
shakehack <sg002498@gmail.com>
2023-06-03 20:49:05 +0530

    having a bar filling bug

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import Expenses from './component/Expenses/Expenses'
+import NewExpense from './component/NewExpense/NewExpense'
+
+const DUMMY = [
+  { id: 'e1', title: 'aloo', date: new Date(2021, 2, 28), amount: '$252' },
+  {
+    id: 'e2',
+    title: 'kanda',
+    date: new Date(2020, 2, 4),
+    amount: '$252',
+  },
+  {
+    id: 'e3',
+    title: 'sweet potato',
+    date: new Date(2021, 5, 25),
+    amount: '$252',
+  },
+  {
+    id: 'e4',
+    title: 'chocolate',
+    date: new Date(2021, 5, 25),
+    amount: '$252',
+  },
+  {
+    id: 'e5',
+    title: 'choco',
+    date: new Date(2023, 5, 25),
+    amount: '$252',
+  },
+];
 
 function App() {
+  
+  const [expenses, setExpenses] = useState(DUMMY)
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
